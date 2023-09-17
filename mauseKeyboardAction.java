30cd52616cc6681329f846dfdb82eaed4f62083f
SATYAM SINGH <92239536+satyamsingh-github@users.noreply.github.com>
2023-09-17 19:27:10 +0530

    Update mauseKeyboardAction.java

--- mauseKeyboardAction.java
+++ mauseKeyboardAction.java
@@ -10,11 +10,11 @@ public class mauseKeyboardAction {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
 
-        driver.navigate().to("https://iitpatna.campus365.io/site/userlogin");
+        driver.navigate().to("link");
         Actions action = new Actions(driver);
 
-        action.moveToElement(driver.findElement(By.xpath("//input[@id='form-username']"))).click().sendKeys("IITP000678").doubleClick().build().perform();
-        action.moveToElement(driver.findElement(By.xpath("//input[@id='form-password']"))).click().sendKeys("moce0b").doubleClick().build().perform();
+        action.moveToElement(driver.findElement(By.xpath("//input[@id='form-username']"))).click().sendKeys("text").doubleClick().build().perform();
+        action.moveToElement(driver.findElement(By.xpath("//input[@id='form-password']"))).click().sendKeys("text").doubleClick().build().perform();
         action.moveToElement(driver.findElement(By.xpath("//button[@id='load1']"))).click().build().perform();
         Thread.sleep(5000);
         action.moveToElement(driver.findElement(By.xpath("//label[@for='1281']"))).click().build().perform();
